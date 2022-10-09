fd6824755de508dd49f08be5da59c469320c4b53
SOURAV DAS <100298703+souravbarisha@users.noreply.github.com>
2022-10-09 16:55:04 +0530

    Add files via upload

---
+++ date-30.09.22/MyProgram/MyProgram.java
@@ -0,0 +1,17 @@
+package com.assignments;
+//Fix the compilation errors and run the program? The program should print “My First Java Program” in the console.
+/*	Class  myProgram {
+Public static void main(String arg[]);{
+system.out.Println("My First Java Program");
+}
+}
+
+Save the file as “MyProgram.java” and compile it    */
+public class MyProgram {      // class name was wrong ->  staring word was small case
+
+	public static void main(String arg[]){
+		System.out.println("My First Java Program");  // 'S' was small case in 'System' and 'P' was small in 'println'
+		}
+
+	
+}
